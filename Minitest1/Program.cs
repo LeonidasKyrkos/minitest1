80634c8c7672ea871a460457809a9921480500c8
LeonidasKyrkos <leo.kyrkos@gmail.com>
2014-02-14 16:00:03 +0000

    printfail

---
+++ Minitest1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minitest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ans = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (((i % 3) == 0) || ((i % 5) == 0))
+                {
+                    ans += i;                    
+                }
+            }
+        }
+    }
+}
